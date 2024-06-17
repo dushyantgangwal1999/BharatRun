875f28cfb64082284f662d401e51d8e961bc69b2
Dushyant Gangwal <dushyantgangwal99@gmail.com>
2024-06-17 16:20:59 +0530

    4.4 Setting up Version Catalog

--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
@@ -33,11 +33,16 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.material3)
+    /** api(libs.androidx.compose.material3) this will help us to access functions of
+    material3 inside the modules that are implementing this designSystem Module.
+     If we have used implementaion(..) then we can use functions of material3
+    but only in designSystem Module
+     **/
+
 }
